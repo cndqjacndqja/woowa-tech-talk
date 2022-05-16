b0f3353caada1bc77266116767c6897c7341b6c6
beomWhale <cndqjacndqja@gmail.com>
2022-05-16 14:37:12 +0900

    init

---
+++ src/test/java/com/example/techtalk/TechTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
