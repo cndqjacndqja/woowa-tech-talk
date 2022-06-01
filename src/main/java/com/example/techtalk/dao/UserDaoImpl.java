08070f687747a034b621b82427598079fd9b3e8f
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-06-01 19:44:09 +0900

    feat: jdbc datasource 이용

--- src/main/java/com/example/techtalk/dao/UserDaoImpl.java
+++ src/main/java/com/example/techtalk/dao/UserDaoImpl.java
@@ -1,33 +1,48 @@
 package com.example.techtalk.dao;
 
-import com.example.techtalk.db.DataBaseConnection;
-import com.example.techtalk.entity.User;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UserDaoImpl implements UserDao{
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import com.example.techtalk.db.DataBaseConnection;
+import com.example.techtalk.entity.User;
+
+public class UserDaoImpl implements UserDao {
+
+    private static final String DATA_BASE_URL = "jdbc:h2:mem:testdb";
+
+    private final DataSource dataSource =
+        new DriverManagerDataSource(DATA_BASE_URL, "sa", "password");
 
     @Override
     public Long save(User user) throws SQLException {
-        Connection con = DataBaseConnection.getConnection();
+        Connection con = null;
         PreparedStatement ps = null;
-        try{
-            String sql = "INSERT INTO user(name) values(?)";
-            String generatedColumns[] = { "ID" };
 
+        try {
+            con = dataSource.getConnection();
+
+            String sql = "INSERT INTO user(name) values(?)";
+            String[] generatedColumns = {"ID"};
             ps = con.prepareStatement(sql, generatedColumns);
             ps.setString(1, user.getName());
+
             ps.execute();
             ResultSet rs = ps.getGeneratedKeys();
             if (rs.next()) {
                 return rs.getLong(1);
             }
             return null;
-        }catch (Exception e) {
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;
+
         } finally {
             if (ps != null) {
                 ps.close();
@@ -40,8 +55,7 @@ public class UserDaoImpl implements UserDao{
 
     @Override
     public User findById(Long id) {
-        Connection con = DataBaseConnection.getConnection();
-        try{
+        try (Connection con = DataBaseConnection.getConnection()) {
             String sql = "SELECT * FROM user WHERE id = ?";
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setLong(1, id);
@@ -52,7 +66,7 @@ public class UserDaoImpl implements UserDao{
                     rs.getString("name"));
             }
             return null;
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;
         }
--- src/main/java/com/example/techtalk/db/DataBaseConnection.java
+++ src/main/java/com/example/techtalk/db/DataBaseConnection.java
@@ -1,16 +1,16 @@
 package com.example.techtalk.db;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 public class DataBaseConnection {
     private static final String DATA_BASE_URL = "jdbc:h2:mem:testdb";
 
     public static Connection getConnection() {
-        try{
-            return DriverManager.getConnection(DATA_BASE_URL, "sa", "password");
-        }catch (Exception exception) {
+        try {
+            return new DriverManagerDataSource(DATA_BASE_URL, "sa", "password").getConnection();
+        } catch (Exception exception) {
             System.out.println(exception.getMessage());
             return null;
         }
